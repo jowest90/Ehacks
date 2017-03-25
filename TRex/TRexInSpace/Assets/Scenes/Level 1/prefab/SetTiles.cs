2838377eb6d43a4d89563d5fb9013907ffbe087b
john west <jowest1990@gmail.com>
2017-03-25 16:37:53 -0500

    add level scenes

---
+++ TRex/TRexInSpace/Assets/Scenes/Level 1/prefab/SetTiles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+RequireComponent(typeof(MeshRenderer))] public class SetTiles : MonoBehaviour { public Vector2 tiling;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().material.mainTextureScale = tiling;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
