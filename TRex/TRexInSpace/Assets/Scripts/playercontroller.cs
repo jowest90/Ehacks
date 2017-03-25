5dd2f6f26e98d3a5fdfdd0534c4323b133c1a1bf
adberry <adamberry295@yahoo.com>
2017-03-25 11:53:30 -0500

    Added title screen

--- TRex/TRexInSpace/Assets/Scripts/playercontroller.cs
+++ TRex/TRexInSpace/Assets/Scripts/playercontroller.cs
@@ -27,7 +27,7 @@ public class playercontroller : MonoBehaviour {
             GameObject lazerbeam = Instantiate(projectile, transform.position, Quaternion.AngleAxis(90, Vector3.forward)) as GameObject;
             lazerbeam.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
             Rigidbody2D rigid = lazerbeam.GetComponent<Rigidbody2D>();
-            transform.Rotate(0, 0, lazerbeam.position.z);
+           // transform.Rotate(0, 0, lazerbeam.position.z);
            // rigid.AddForce(transform.forward * speed);
            // Physics.IgnoreCollision(rigid.GetComponent<Collider2D>, playercontroller.GetComponent);
         }
