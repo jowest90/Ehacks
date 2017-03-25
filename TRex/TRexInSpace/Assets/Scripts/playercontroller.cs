d7fc0eaa6ea93c7f582a4c4c68e320242e11ccb8
adberry <adamberry295@yahoo.com>
2017-03-25 15:09:18 -0500

    Added basic jump
    Need to fix it so it will not fly out of the camera's range.

--- TRex/TRexInSpace/Assets/Scripts/playercontroller.cs
+++ TRex/TRexInSpace/Assets/Scripts/playercontroller.cs
@@ -8,28 +8,33 @@ public class playercontroller : MonoBehaviour {
     public float projectileSpeed;
     public GameObject projectile;
     public float firingRate = .2f;
+    public Vector3 jump;
+    public float jumpForce = 1f;
+    public bool isGrounded;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-
-   
-	
+        jump = new Vector3(0, 1.2f, 0);
+        isGrounded = true;
+    }
+       
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-           // InvokeRepeating("FireProjectile", .000001f, firingRate);
+            // InvokeRepeating("FireProjectile", .000001f, firingRate);
+           
             GameObject lazerbeam = Instantiate(projectile, transform.position, Quaternion.AngleAxis(90, Vector3.forward)) as GameObject;
             Rigidbody2D rigid = lazerbeam.GetComponent<Rigidbody2D>();
             rigid.AddForce(transform.forward * speed);
             lazerbeam.GetComponent<Rigidbody2D>().position = new Vector3(transform.position.x + 1, transform.position.y - .15f);
             lazerbeam.GetComponent<Rigidbody2D>().velocity = new Vector3(5, projectileSpeed);
-           
+
             //transform.translate(Vector3.forward Time.deltaTime speed);
             // transform.Rotate(0, 0, lazerbeam.position.z);
-             
-            // Physics.IgnoreCollision(rigid.GetComponent<Collider2D>, playercontroller.GetComponent);
+
+            Destroy(lazerbeam, 3.0f); // remove lazers
         }
 
         if (Input.GetKey(KeyCode.E))
@@ -45,5 +50,15 @@ public class playercontroller : MonoBehaviour {
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+        {
+            Rigidbody2D rigbody = GetComponent<Rigidbody2D>();
+            rigbody.AddForce(jump, ForceMode2D.Impulse);
+            isGrounded = false;
+            yield WaitForSeconds(3);
+            isGrounded = true;
+        }
+        
     }
 }
