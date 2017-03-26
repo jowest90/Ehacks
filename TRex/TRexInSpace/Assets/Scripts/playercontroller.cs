9c005ae1aa2ed088ad44865130cf462d9385b30e
john west <jowest1990@gmail.com>
2017-03-26 00:58:52 -0500

    blah

--- TRex/TRexInSpace/Assets/Scripts/playercontroller.cs
+++ TRex/TRexInSpace/Assets/Scripts/playercontroller.cs
@@ -4,35 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class playercontroller : MonoBehaviour {
-<<<<<<< HEAD
-  public float speed = 1.0f;
-  public GameObject projectile;
-  public float projectileSpeed;
-
-
-	// Use this for initialization
-	void Start () {
-
-  }
-
-	// Update is called once per frame
-	void Update () {
-    if(Input.GetKeyDown(KeyCode.Mouse0)){
-      GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-      beam.Rigidbody2D.velocity = new Vector3(0,projectileSpeed,0);
-    }
-
-    if(Input.GetKey(KeyCode.LeftArrow)){
-      transform.position += new Vector3(-speed * Time.deltaTime,0,0);
-    }
-     else if (Input.GetKey(KeyCode.RightArrow)){
-      transform.position += new Vector3(speed* Time.deltaTime,0,0);
-    }
-  }
-
-
-=======
-    public float speed = 2.0f;
+public float speed = 2.0f;
     public int curhealth;
     public float projectileSpeed;
     public GameObject projectile;
@@ -171,6 +143,4 @@ public class playercontroller : MonoBehaviour {
     //    if(collision.gameObject.tag == "Player")
     //    Destroy(collision.gameObject);
     //}
-
->>>>>>> refs/remotes/origin/Adam-branch
 }
