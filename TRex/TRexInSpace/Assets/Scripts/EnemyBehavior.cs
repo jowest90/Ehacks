722562c9071a711ede171e19619b85c55f5ebc9e
john west <jowest1990@gmail.com>
2017-03-25 23:03:47 -0500

    trying to get damge to work

--- TRex/TRexInSpace/Assets/Scripts/EnemyBehavior.cs
+++ TRex/TRexInSpace/Assets/Scripts/EnemyBehavior.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
+	public float health = 150f;
 	int waitingTime =2;
 	 float timer;
 	 public GameObject projectile;
@@ -35,6 +36,16 @@ public class EnemyBehavior : MonoBehaviour {
 			  Destroy(lazerbeam, 2.0f);
         timer = 0;
      }
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			health -=missile.getDamage();
+			if(health <=0){
+				Destroy(gameObject);
+			}
+		}
 	}
 
 }
